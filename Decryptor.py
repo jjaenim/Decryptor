c787dcf649590ec9f656827d74d453dd8af319ac
jjaenim <joshuatead@gmail.com>
2023-04-21 20:22:34 +0800

    Asks the user to input encrypted text

--- Decryptor.py
+++ Decryptor.py
@@ -1,16 +1,19 @@
-# Daet, Joshua Adrian O.
-# Assignment No. 2
-# Question No. 2
+# Ask the user to input the enrypted text
+input_str = input("Enter the enrypted text: ")
 
-# This program will decrypt a encrypted text
-# Create a header
-print("")
-name = "Hi! I am Joshua Adrian O. Daet of BSCpE 1-4"
-print(name.center(158))
+output_str = ''  
 
-sentence = "This is a program where you can decrypt a encrypted text"
-print("")
-print(sentence.rjust(106))
-print("")
-print("-------------------------------------------------------------------------------------------------------------------------------------------------------------")
+for i in range(len(input_str)):
+    if input_str[i] == "*":
+        output_str += "a"
+    elif input_str[i] == "&":
+        output_str += "e"
+    elif input_str[i] == "#":
+        output_str += "i"
+    elif input_str[i] == "+":
+        output_str += "o"
+    elif input_str[i] == "!":
+        output_str += "u"
+    else:
+        output_str += input_str[i]
 print("")
