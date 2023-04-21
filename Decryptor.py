a4bc5efbc684d8de256c49ada3410d8eee364279
jjaenim <joshuatead@gmail.com>
2023-04-21 20:20:52 +0800

    Creating the header

---
+++ Decryptor.py
@@ -0,0 +1,16 @@
+# Daet, Joshua Adrian O.
+# Assignment No. 2
+# Question No. 2
+
+# This program will decrypt a encrypted text
+# Create a header
+print("")
+name = "Hi! I am Joshua Adrian O. Daet of BSCpE 1-4"
+print(name.center(158))
+
+sentence = "This is a program where you can decrypt a encrypted text"
+print("")
+print(sentence.rjust(106))
+print("")
+print("-------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("")
