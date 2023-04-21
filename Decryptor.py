1b50017f8948887abb6c1f018b76e45782f3e2c8
jjaenim <joshuatead@gmail.com>
2023-04-21 20:23:26 +0800

    Prints the output

--- Decryptor.py
+++ Decryptor.py
@@ -1,19 +1,3 @@
-# Ask the user to input the enrypted text
-input_str = input("Enter the enrypted text: ")
-
-output_str = ''  
-
-for i in range(len(input_str)):
-    if input_str[i] == "*":
-        output_str += "a"
-    elif input_str[i] == "&":
-        output_str += "e"
-    elif input_str[i] == "#":
-        output_str += "i"
-    elif input_str[i] == "+":
-        output_str += "o"
-    elif input_str[i] == "!":
-        output_str += "u"
-    else:
-        output_str += input_str[i]
+# Ouput of the decrypted text
+print("The decrypted text is: " +output_str)
 print("")
